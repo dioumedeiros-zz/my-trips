4df0c66eaeb3ae09245a38ede669786175562bc9
Diou Medeiros <diou.182@gmail.com>
2021-09-12 19:46:23 -0300

    :sparkles: Initial commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,3 @@
-import * as S from './styles'
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo  e React Avaçado escrito ao lado."
-    />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>Typescritp, ReactJS, NextJS</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para o computador"
-    />
-  </S.Wrapper>
-)
+const Main = () => <h1>NextJS na prática</h1>
 
 export default Main
